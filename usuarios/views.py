4c856958a2f8a279e76b0339b61fc64ca6522077
Felipe Oliveira <flipeaz342@gmail.com>
2024-01-17 19:37:01 -0400

    fix: i changed the login and registration views

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse
-from django.shortcuts import render, redirect
 from django.contrib.messages import constants
 from django.contrib import messages
 from django.core.exceptions import ValidationError
@@ -16,22 +14,14 @@ def cadastro(request):
         form = CadastroForm(request.POST)
         if form.is_valid():
             try:
-                user = form.save(commit=False)
-                if user.password1 != user.password2:
-                    messages.add_message(request, constants.ERROR, 'As senhas não conferem')
-                user.username = user.username
-                user.save()
+                user = form.save()
                 login(request, user)
-                messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado com sucesso')
-                return redirect('/flashcard/novo_flashcard')
+                messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado com sucesso!')
+                return redirect('novo_flashcard')
             
             except ValidationError as e:
                 messages.add_message(request, constants.ERROR, str(e))
-                return redirect(reverse('cadastro'))
-            
-            except Exception:
-                messages.add_message(request, constants.ERROR, 'Erro interno do servidor')
-                return redirect(reverse('cadastro'))
+                return redirect('cadastro')
 
     context = {
         'form': form
@@ -43,22 +33,13 @@ def pageLogin(request):
     form = LoginForm()
 
     if request.user.is_authenticated:
-        return redirect(reverse('flashcard'))
+        return redirect('novo_flashcard')
     
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-            try:
-                user = User.objects.get(username=username)
-            except User.DoesNotExist:
-                messages.add_messsage(
-                    request,
-                    constants.ERROR,
-                    'O usuário não existe'
-                )
-                return redirect(reverse('login'))
 
             user = authenticate(
                 request,
@@ -68,7 +49,7 @@ def pageLogin(request):
 
             if user is not None:
                 login(request, user)
-                return redirect('flashcard')
+                return redirect('novo_flashcard')
             else:
                 messages.add_message(
                     request,
@@ -84,5 +65,4 @@ def pageLogin(request):
 
 def logoutUser(request):
     logout(request)
-    messages.add_message(request, constants.SUCCESS, 'Deslogado com sucesso')
-    return render(request, 'login.html')
+    return redirect('login')
