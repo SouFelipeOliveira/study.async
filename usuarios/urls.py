24a1cca3ab7dc3ed7c701168c92ab9ad637b7bd4
Felipe Oliveira <flipeaz342@gmail.com>
2024-01-17 19:33:29 -0400

    added the correct url for logout

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('cadastro/', views.cadastro, name='cadastro'),
     path('login/', views.pageLogin, name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('logout/', views.logoutUser, name='logout'),
 ]
