2bac00d19e7db0d75e2f21943596c3acd4318a8d
Felipe Oliveira <flipeaz342@gmail.com>
2024-01-17 21:58:22 -0400

    I added the flashcard app

---
+++ flashcard/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('novo_flashcard/', views.novo_flashcard, name='novo_flashcard'),
+]
--- flashcard/views.py
+++ flashcard/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Categoria, Flashcard
+from django.contrib import messages
+from django.contrib.messages import constants
 
-# Create your views here.
+@login_required(login_url='login')
+def novo_flashcard(request):
+    dificuldades = Flashcard.DIFICULDADE_CHOICES
+    categorias = Categoria.objects.all()
+
+    flashcards = Flashcard.objects.filter(user_id=request.user.id)
+
+    if request.method == 'POST':
+        pergunta = request.POST.get('pergunta')
+        resposta = request.POST.get('resposta')
+        categoria = request.POST.get('categoria')
+        dificuldade = request.POST.get('dificuldade')
+
+        if len(pergunta.strip()) == 0 or len(resposta.strip()) == 0:
+            messages.add_message(
+                request,
+                constants.ERROR,
+                "Preencha os campos de pergunta e resposta"
+            )
+            return redirect('novo_flashcard')
+
+        Flashcard.objects.create(
+            user=request.user,
+            pergunta=pergunta,
+            resposta=resposta,
+            categoria_id=categoria,
+            dificuldade=dificuldade
+        )
+        messages.add_message(
+            request,
+            messages.SUCCESS,
+            'Flashcard criado com sucesso'
+        )
+        return redirect('novo_flashcard')
+    
+    context = {
+        'dificuldades': dificuldades,
+        'categorias': categorias
+    }
+    return render(request, 'novo_flashcard.html', context)
