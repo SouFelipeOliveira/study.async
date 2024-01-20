13e20cb05e9a686dc8626a870ae6b16a751614bc
Felipe Oliveira <flipeaz342@gmail.com>
2024-01-19 22:13:10 -0400

    added challenge models

--- flashcard/models.py
+++ flashcard/models.py
@@ -19,3 +19,26 @@ class Flashcard(models.Model):
 
     def __str__(self):
         return self.pergunta
+    
+
+class FlashcardDesafio(models.Model):
+    flashcard = models.ForeignKey(Flashcard, on_delete=models.DO_NOTHING)
+    respondido = models.BooleanField(default=False)
+    acertou = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.flashcard.pergunta
+
+
+class Desafio(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    titulo = models.CharField(max_length=100)
+    categoria = models.ManyToManyField(Categoria)
+    quantidade_perguntas = models.IntegerField()
+    dificuldade = models.CharField(
+        max_length=1, choices=Flashcard.DIFICULDADE_CHOICES
+    )
+    flashcards = models.ManyToManyField(FlashcardDesafio)
+
+    def __str__(self):
+        return self.titulo
